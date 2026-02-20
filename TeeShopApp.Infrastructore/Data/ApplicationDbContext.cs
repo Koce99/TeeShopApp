0701f205768eaebfa9f0e40abba55708518f4b41
helenasimova <h.simova2020@pmg-pernik.com>
2026-02-20 16:10:18 +0200

    Initial

---
+++ TeeShopApp.Infrastructore/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using TeeShopApp.Infrastructure.Data.Domain;
+
+namespace TeeShopApp.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
